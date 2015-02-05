5693a73fdf4b24815573a29ad1c06d51b92a8b2b
Alexei Nunez <alexeirnunez@gmail.com>
2015-02-04 22:48:13 -0600

    fix build

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,14 @@ var gulp = require('gulp'),
   watch = require('gulp-watch'),
   coffeeify = require('coffeeify'),
   browserify = require('browserify'),
-  source = require('vinyl-source-stream'),
-  to5 = require('6to5ify').configure({
-    blacklist: ['useStrict']
-  });
+  source = require('vinyl-source-stream')
 
 gulp.task('trello', function() {
   return browserify()
     .transform(coffeeify)
-    .add('./src/trello/main.coffee')
+    .add('./src/trello/add_card.coffee')
     .bundle()
-    .pipe(source('main.js'))
+    .pipe(source('add_card.js'))
     .pipe(gulp.dest('./dist/trello/'));
 });
 
